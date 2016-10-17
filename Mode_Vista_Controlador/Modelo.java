92c3fad74c0705f8e0ca13cb79b626526bfb61ca
JesusOrtegaVilchez <jesus_a_t_g@hotmail.com>
2016-10-17 18:43:58 +0200

    Create Modelo.java

---
+++ Mode_Vista_Controlador/Modelo.java
@@ -0,0 +1,14 @@
+package Modelo_vista_controlador;
+
+public class Modelo {
+	public double sumar(double operando1, double operando2){
+		double suma = operando1 + operando2;
+		return suma;
+	}
+	
+	public double restar ( double operando1, double operando2){
+		double restar = operando1 - operando2;
+		return restar;
+
+	}
+}
